2e426d031b225154b70a123a0fa16016d09b9d0f
bwang7 <bwang7@ebay.com>
2024-05-24 15:40:14 -0700

    add ddpq

--- PolicyGradient/ddpg.py
+++ PolicyGradient/ddpg.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import numpy as np
 import collections
 from tqdm import tqdm
+import random
 
 
 class ReplayBuffer:
@@ -24,6 +25,12 @@ class ReplayBuffer:
 
 class PolicyNet(torch.nn.Module):
     def __init__(self, state_dim, hidden_dim, action_dim, action_bound):
+        """
+        :param state_dim: the dimension of the state space
+        :param hidden_dim: the dimension of the hidden layer
+        :param action_dim: the dimension of the action space
+        :param action_bound: the largest acceptable value of the action
+        """
         super().__init__()
         self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
         self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
@@ -49,27 +56,33 @@ class QValueNet(torch.nn.Module):
 
 
 class DDPG:
-    def __init__(self, state_dim, hidden_dim, action_dim, action_bound, sigma, actor_lr, critic_lr, tau, gamma, device):
+    """
+    Deep Deterministic Policy Gradient(DDPG) algorithm: off-policy algo that is able to handle continuous action space.
+    it combines the advantages of both policy gradient and value-based methods. The critic network learns the Q value,
+    like DQN, and the actor network learns the policy. The reason that actor network also needs a target network is that
+    actor network result is part of the input to calculate the target Q value, as can be seen in the update function.
+    """
+    def __init__(self, env, state_dim, hidden_dim, action_dim, action_bound, sigma, actor_lr, critic_lr, tau, gamma,
+                 device):
+        self.env = env
         self.actor = PolicyNet(state_dim, hidden_dim, action_dim, action_bound).to(device)
         self.critic = QValueNet(state_dim, hidden_dim, action_dim).to(device)
         self.target_actor = PolicyNet(state_dim, hidden_dim, action_dim, action_bound).to(device)
         self.target_critic = QValueNet(state_dim, hidden_dim, action_dim).to(device)
-        # 初始化目标价值网络并设置和价值网络相同的参数
         self.target_critic.load_state_dict(self.critic.state_dict())
-        # 初始化目标策略网络并设置和策略相同的参数
         self.target_actor.load_state_dict(self.actor.state_dict())
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)
         self.gamma = gamma
-        self.sigma = sigma  # 高斯噪声的标准差,均值直接设为0
-        self.tau = tau  # 目标网络软更新参数
+        self.sigma = sigma
+        self.tau = tau
         self.action_dim = action_dim
         self.device = device
+        self.return_list = []
 
     def take_action(self, state):
         state = torch.tensor(np.array([state]), dtype=torch.float).to(self.device)
         action = self.actor(state).item()
-        # 给动作添加噪声，增加探索
         action = action + self.sigma * np.random.randn(self.action_dim)
         return action
 
@@ -77,87 +90,45 @@ class DDPG:
         for param_target, param in zip(target_net.parameters(), net.parameters()):
             param_target.data.copy_(param_target.data * (1.0 - self.tau) + param.data * self.tau)
 
-    def update(self, transition_dict):
-        states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
-        actions = torch.tensor(transition_dict['actions'], dtype=torch.float).view(-1, 1).to(self.device)
-        rewards = torch.tensor(transition_dict['rewards'], dtype=torch.float).view(-1, 1).to(self.device)
-        next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(self.device)
-        dones = torch.tensor(transition_dict['dones'], dtype=torch.float).view(-1, 1).to(self.device)
+    def update(self, states, actions, rewards, next_states, dones):
+        states = torch.tensor(states, dtype=torch.float).to(self.device)
+        actions = torch.tensor(actions, dtype=torch.float).view(-1, 1).to(self.device)
+        rewards = torch.tensor(rewards, dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(next_states, dtype=torch.float).to(self.device)
+        dones = torch.tensor(dones, dtype=torch.float).view(-1, 1).to(self.device)
 
-        next_q_values = self.target_critic(next_states, self.target_actor(next_states))
+        next_q_values = self.target_critic(next_states, self.target_actor(next_states))  # why actor needs target net
         q_targets = rewards + self.gamma * next_q_values * (1 - dones)
         critic_loss = torch.mean(F.mse_loss(self.critic(states, actions), q_targets))
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        actor_loss = -torch.mean(self.critic(states, self.actor(states)))
+        actor_loss = -torch.mean(self.critic(states, self.actor(states)))  # the goal is to maximize Q value
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
 
-        self.soft_update(self.actor, self.target_actor)  # 软更新策略网络
-        self.soft_update(self.critic, self.target_critic)  # 软更新价值网络
+        self.soft_update(self.actor, self.target_actor)
+        self.soft_update(self.critic, self.target_critic)
 
-
-def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
-    return_list = []
-    for i in range(10):
-        with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
-            for i_episode in range(int(num_episodes/10)):
-                episode_return = 0
-                state, info = env.reset()
-                done = False
-                truncated = False
-                while not done and not truncated:
-                    action = agent.take_action(state)
-                    next_state, reward, done, truncated, info = env.step(action)
-                    replay_buffer.add(state, action, reward, next_state, done)
-                    state = next_state
-                    episode_return += reward
-                    if replay_buffer.size() > minimal_size:
-                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
-                        transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r, 'dones': b_d}
-                        agent.update(transition_dict)
-                return_list.append(episode_return)
-                if (i_episode+1) % 10 == 0:
-                    pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
-                pbar.update(1)
-    return return_list
-
-
-if __name__ == '__main__':
-    import torch
-    import gymnasium as gym
-    import random
-    import numpy as np
-    from reinforce import REINFORCE
-    from actor_critic import ActorCritic
-    from ppo import PPO
-    from util import plot_reward
-    import matplotlib.pyplot as plt
-
-    actor_lr = 3e-4
-    critic_lr = 3e-3
-    num_episodes = 200
-    hidden_dim = 64
-    gamma = 0.98
-    tau = 0.005  # 软更新参数
-    buffer_size = 10000
-    minimal_size = 1000
-    batch_size = 64
-    sigma = 0.01  # 高斯噪声标准差
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
-    env_name = 'Pendulum-v1'
-    env = gym.make(env_name)
-    random.seed(0)
-    np.random.seed(0)
-    torch.manual_seed(0)
-    replay_buffer = ReplayBuffer(buffer_size)
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
-    action_bound = env.action_space.high[0]  # 动作最大值
-    agent = DDPG(state_dim, hidden_dim, action_dim, action_bound, sigma, actor_lr, critic_lr, tau, gamma, device)
-
-    return_list = train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size)
+    def train(self, replay_buffer, num_episodes, minimal_size, batch_size):
+        for i in range(num_episodes):
+            episode_return = 0
+            state, info = self.env.reset()
+            done = False
+            truncated = False
+            while not done and not truncated:
+                action = self.take_action(state)
+                next_state, reward, done, truncated, info = self.env.step(action)
+                replay_buffer.add(state, action, reward, next_state, done)
+                state = next_state
+                episode_return += reward
+                if replay_buffer.size() > minimal_size:
+                    batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones = \
+                        replay_buffer.sample(batch_size)
+                    self.update(batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones)
+            self.return_list.append(episode_return)
+            if (i + 1) % 10 == 0:
+                print("Average reward for the last 10 episodes with "
+                      "from {} to {} is: {}".format(i - 9, i + 1, np.mean(self.return_list[-10:])))
--- PolicyGradient/train.py
+++ PolicyGradient/train.py
@@ -5,6 +5,7 @@ import numpy as np
 from reinforce import REINFORCE
 from actor_critic import ActorCritic
 from ppo import PPO, PPOContinuous
+from ddpg import DDPG, ReplayBuffer
 from util import plot_reward
 import matplotlib.pyplot as plt
 
@@ -25,18 +26,33 @@ cartpole_params = {
     'eps': 0.2
 }
 
-pendulum_params = {
+ppo_pendulum_params = {
     'env_name': 'Pendulum-v1',
     'num_episodes': 2000,
     'hidden_dim': 128,
     'gamma': 0.9,
     'actor_lr': 1e-4,
     'critic_lr': 5e-3,
-    # for PPO
     'lmbda': 0.9,
     'epochs': 10,
     'eps': 0.2
 }
+
+ddpg_pendulum_params = {
+    'env_name': 'Pendulum-v1',
+    'actor_lr': 3e-4,
+    'critic_lr': 3e-3,
+    'num_episodes': 200,
+    'hidden_dim': 64,
+    'gamma': 0.98,
+    'tau': 0.005,
+    'buffer_size': 10000,
+    'minimal_size': 1000,
+    'batch_size': 64,
+    'sigma': 0.01
+
+}
+
 
 if __name__ == '__main__':
     SEED = 0
@@ -51,10 +67,10 @@ if __name__ == '__main__':
     epochs = cartpole_params['epochs']
     eps = cartpole_params['eps']
 
-    # create the environment
+    # try different approaches on CartPole-v1
     env_name = cartpole_params['env_name']
     env = gym.make(env_name)
-    """
+
     # set random seed
     random.seed(SEED)
     np.random.seed(SEED)
@@ -90,30 +106,60 @@ if __name__ == '__main__':
     plt.title('Comparison of Reinforce, Actor Critic and PPO')
     plt.legend()
     plt.show()
-    """
 
-    # run the code with Pendulum-v1
-    actor_lr = pendulum_params['actor_lr']
-    critic_lr = pendulum_params['critic_lr']
-    num_episodes = pendulum_params['num_episodes']
-    hidden_dim = pendulum_params['hidden_dim']
-    gamma = pendulum_params['gamma']
-    lmbda = pendulum_params['lmbda']
-    epochs = pendulum_params['epochs']
-    eps = pendulum_params['eps']
-
-    env_name = pendulum_params['env_name']
+    # try different approaches on Pendulum-v1
+    env_name = ppo_pendulum_params['env_name']
     env = gym.make(env_name)
     random.seed(SEED)
     np.random.seed(SEED)
     torch.manual_seed(SEED)
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
-    agent_ppo_c = PPOContinuous(env=env, state_dim=state_dim, hidden_dim=hidden_dim, action_dim=action_dim,
-                                actor_lr=actor_lr, critic_lr=critic_lr, lmbda=lmbda, epochs=epochs, epsilon=eps,
-                                gamma=gamma, device=device)
+    action_bound = env.action_space.high[0]
+    agent_ppo_c = PPOContinuous(env=env,
+                                state_dim=state_dim,
+                                hidden_dim=hidden_dim,
+                                action_dim=action_dim,
+                                actor_lr=ppo_pendulum_params['actor_lr'],
+                                critic_lr=ppo_pendulum_params['critic_lr'],
+                                lmbda=ppo_pendulum_params['lmbda'],
+                                epochs=ppo_pendulum_params['epochs'],
+                                epsilon=ppo_pendulum_params['eps'],
+                                gamma=ppo_pendulum_params['gamma'],
+                                device=device)
     agent_ppo_c.train(num_episodes)
     plot_reward(reward_list=agent_ppo_c.return_list, title="PPO_Continuous")
+
+    replay_buffer = ReplayBuffer(ddpg_pendulum_params['buffer_size'])
+    agent_ddpg = DDPG(env=env,
+                        state_dim=state_dim,
+                        hidden_dim=hidden_dim,
+                        action_dim=action_dim,
+                        action_bound=action_bound,
+                        actor_lr=ddpg_pendulum_params['actor_lr'],
+                        critic_lr=ddpg_pendulum_params['critic_lr'],
+                        gamma=ddpg_pendulum_params['gamma'],
+                        tau=ddpg_pendulum_params['tau'],
+                        sigma=ddpg_pendulum_params['sigma'],
+                        device=device)
+    agent_ddpg.train(replay_buffer, num_episodes, ddpg_pendulum_params['minimal_size'],
+                     ddpg_pendulum_params['batch_size'])
+    plot_reward(reward_list=agent_ddpg.return_list, title="DDPG")
+
+    # play the game with the trained agent
+    ppo_c_reward = agent_ppo_c.play(num_episodes=100)
+    ddpg_reward = agent_ddpg.play(num_episodes=100)
+    # plot the rewards in the same plot
+    plt.plot(ppo_c_reward, label='PPO_Continuous')
+    plt.plot(ddpg_reward, label='DDPQ')
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
+    plt.title('Comparison of PPO and DDPQ')
+    plt.legend()
+    plt.show()
+
+
+
 
 
 
